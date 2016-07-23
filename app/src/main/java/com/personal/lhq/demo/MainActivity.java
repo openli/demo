940e2aaac4fa281d5355efb14e9d1c8225a3f19b
Li Hanqing <tclihanqing@gmail.com>
2016-07-24 00:55:31 +0800

    Initial commit

---
+++ app/src/main/java/com/personal/lhq/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.personal.lhq.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static String TAG = "MainActivity";
+
+    private String FG="s";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
